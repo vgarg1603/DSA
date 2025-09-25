a8742cfa6807a31e4498d1335df41f711a7f5bbb
Vipul Garg <vgarg1603@gmail.com>
2025-09-25 23:48:31 +0530

    subsequenceSum

--- Recursions/03_subsequencePrint.cpp
+++ Recursions/03_subsequencePrint.cpp
@@ -24,6 +24,57 @@ void printSubsequence(vector<int> &arr, int n, vector<int> &sub, vector<vector<i
     printSubsequence(arr, n, sub, subsequences, i + 1); // Move ahead without taking
 }
 
+void subsequenceWithSumK(vector<int> &arr, int n, vector<int> &sub, int i, int sum, int k)
+{
+    if (i >= n)
+    {
+        if (sum == k)
+        {
+            for (int i = 0; i < (int)sub.size(); i++)
+            {
+                cout << sub[i] << " ";
+            }
+            cout << "\n";
+        }
+        return;
+    }
+
+    sub.push_back(arr[i]);
+    sum += arr[i];
+    subsequenceWithSumK(arr, n, sub, i + 1, sum, k);
+    sub.pop_back();
+    sum -= arr[i];
+    subsequenceWithSumK(arr, n, sub, i + 1, sum, k);
+}
+
+bool AnySubsequenceWithSumK(vector<int>& arr, int n, vector<int>& sub, int i, int sum, int k) {
+    if (i >= n)
+    {
+        if (sum == k)
+        {
+            for (int i = 0; i < (int)sub.size(); i++)
+            {
+                cout << sub[i] << " ";
+            }
+            cout << "\n";
+            return true;
+        }
+        return false;
+    }
+
+    sub.push_back(arr[i]);
+    sum += arr[i];
+
+    if(AnySubsequenceWithSumK(arr, n, sub, i+1, sum, k)) {
+        return true;
+    }
+
+    sub.pop_back();
+    sum -= arr[i];
+
+    return AnySubsequenceWithSumK(arr, n, sub, i+1, sum, k);
+}
+
 int main()
 {
     ios_base::sync_with_stdio(false);
@@ -37,28 +88,33 @@ int main()
 
     vector<vector<int>> subsequences;
     vector<int> sub;
-    printSubsequence(arr, n, sub, subsequences, 0);
+    // printSubsequence(arr, n, sub, subsequences, 0);
 
-   
-    cout << "All subsequences (total = " << subsequences.size() << "):\n";
-    for (int i = 0; i < (int)subsequences.size(); i++)
-    {
-        if (subsequences[i].empty())
-        {
-            cout << "{}";
-        }
-        else
-        {
-            cout << "{";
-            for (int j = 0; j < (int)subsequences[i].size(); j++)
-            {
-                cout << subsequences[i][j];
-                if (j + 1 < (int)subsequences[i].size()) cout << ' ';
-            }
-            cout << "}";
-        }
-        cout << "\n";
-    }
+    // cout << "All subsequences (total = " << subsequences.size() << "):\n";
+    // for (int i = 0; i < (int)subsequences.size(); i++)
+    // {
+    //     if (subsequences[i].empty())
+    //     {
+    //         cout << "{}";
+    //     }
+    //     else
+    //     {
+    //         cout << "{";
+    //         for (int j = 0; j < (int)subsequences[i].size(); j++)
+    //         {
+    //             cout << subsequences[i][j];
+    //             if (j + 1 < (int)subsequences[i].size()) cout << ' ';
+    //         }
+    //         cout << "}";
+    //     }
+    //     cout << "\n";
+    // }
+
+    int k = 2;
+
+    // subsequenceWithSumK(arr, n, sub, 0, 0, k);
+
+    AnySubsequenceWithSumK(arr, n, sub, 0, 0, k);
 
     return 0;
 }
